03eee5c9f243f4f7cab4f1dd2234e1ab4ca42ad7
Balázs Gábor <mail.gabor.balazs@gmail.com>
2016-10-10 19:52:30 +0200

    Regex test

--- src/main/java/hu/gaborbalazs/App.java
+++ src/main/java/hu/gaborbalazs/App.java
@@ -3,7 +3,9 @@ package hu.gaborbalazs;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		String text = "dsfworld\ndsdsd\nfdsf\ndkld\ndkfj_dfdf";
+		System.out.println(text.matches("(.|\\s)*?world(.|\\s)*?"));
 
 	}
 }
