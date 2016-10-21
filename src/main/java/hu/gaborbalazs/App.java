07915fdec9e0ae4005a9e84ef3e2f69f6122f4bc
Balázs Gábor <mail.gabor.balazs@gmail.com>
2016-10-22 00:15:54 +0200

    Some changes in Pom file for packaging

--- src/main/java/hu/gaborbalazs/App.java
+++ src/main/java/hu/gaborbalazs/App.java
@@ -3,9 +3,7 @@ package hu.gaborbalazs;
 public class App {
 
 	public static void main(String[] args) {
-
-		String text = "dsfworld\ndsdsd\nfdsf\ndkld\ndkfj_dfdf";
-		System.out.println(text.matches("(.|\\s)*?world(.|\\s)*?"));
-
+		
+		System.out.println("Hello World");
 	}
 }
