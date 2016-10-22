a1801bf43bbd17ef896d109a21a16e5327982de6
Balázs Gábor <mail.gabor.balazs@gmail.com>
2016-10-22 17:57:35 +0200

    Equals and hashCode

--- src/main/java/hu/gaborbalazs/App.java
+++ src/main/java/hu/gaborbalazs/App.java
@@ -1,9 +1,20 @@
 package hu.gaborbalazs;
 
+import java.util.HashSet;
+
 public class App {
 	
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World");
+		
+		Person p1 = new Person(123);
+		Person p2 = new Person(123);
+		
+		HashSet<Person> set = new HashSet<Person>();
+		
+		set.add(p1);
+		
+		System.out.println(set.contains(p2));
 	}
 }
