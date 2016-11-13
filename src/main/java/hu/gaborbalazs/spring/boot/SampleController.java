71e36463b4bf8b6f4ef4e57f42326f33d3e705eb
Balázs Gábor <mail.gabor.balazs@gmail.com>
2016-11-13 22:33:04 +0100

    Swagger

--- src/main/java/hu/gaborbalazs/spring/boot/SampleController.java
+++ src/main/java/hu/gaborbalazs/spring/boot/SampleController.java
@@ -3,17 +3,27 @@ package hu.gaborbalazs.spring.boot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.*;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
 @RestController
-@EnableAutoConfiguration
+@SpringBootApplication
 @ComponentScan
 public class SampleController {
 	
 	@Autowired
 	private TestService service;
-
+	
     @RequestMapping("/")
     public String home() {
         return "Hello World!";
@@ -32,6 +42,15 @@ public class SampleController {
     @RequestMapping("/add")
     public int add() {
     	return service.add(10, 20);
+    }
+    
+    @Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.any())              
+          .paths(PathSelectors.any())                          
+          .build();                                           
     }
 
     public static void main(String[] args) throws Exception {
