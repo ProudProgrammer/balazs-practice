6229d6c391b8b7b899b56245050e96ca28305327
Balázs Gábor <mail.gabor.balazs@gmail.com>
2017-06-20 00:20:09 +0200

    Nested loop with lambda

---
+++ src/main/java/hu/gaborbalazs/lambda/NestedLoopWithLambda.java
@@ -0,0 +1,48 @@
+package hu.gaborbalazs.lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class NestedLoopWithLambda {
+
+	public static void main(String[] args) {
+		
+		List<TestClass> testClassList = new ArrayList<>();
+		testClassList.add(new TestClass(1, "text1"));
+		testClassList.add(new TestClass(2, "text2"));
+		testClassList.add(new TestClass(3, "text3"));
+		testClassList.add(new TestClass(4, "text4"));
+		testClassList.add(new TestClass(5, "text5"));
+		
+		List<Integer> idList = new ArrayList<>();
+		idList.add(5);
+		idList.add(1);
+		idList.add(4);
+		
+		String texts = testClassList.stream().filter(e -> idList.contains(e.getId())).map(e -> e.getText()).collect(Collectors.joining(","));
+		System.out.println(texts);
+	}
+
+}
+
+class TestClass {
+	private int id;
+	private String text;
+	public TestClass(int id, String text) {
+		this.id = id;
+		this.text = text;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+}
---
+++ src/main/java/hu/gaborbalazs/test/App.java
@@ -0,0 +1,11 @@
+package hu.gaborbalazs.test;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		System.out.println(">> main()");
+		
+		System.out.println("<< main()");
+	}
+}
