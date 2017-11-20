030f74b4c2eacc849d78bd14896fba1d1904a58c
Balázs Gábor <mail.gabor.balazs@gmail.com>
2017-11-20 20:02:58 +0100

    Some Generics tricks

---
+++ src/main/java/hu/gaborbalazs/generics/App2.java
@@ -0,0 +1,32 @@
+package hu.gaborbalazs.generics;
+
+public class App2 {
+
+	public enum Type {
+		STRING, INTEGER;
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println(">> main()");
+
+		String s = method(Type.STRING);
+		Integer i = method(Type.INTEGER);
+		System.out.println("s: " + s);
+		System.out.println("i: " + i);
+
+		System.out.println("<< main()");
+	}
+
+	@SuppressWarnings("unchecked")
+	public static <T> T method(Type type) {
+		switch (type) {
+		case STRING:
+			return (T) new String("hello");
+		case INTEGER:
+			return (T) new Integer(5);
+		default:
+			return null;
+		}
+	}
+}
