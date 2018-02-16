4efcdf51fc4508adae64f8f98c06be1dc3be343e
Balázs Gábor <mail.gabor.balazs@gmail.com>
2018-02-16 11:13:51 +0100

    Fix regexp

--- src/main/java/hu/gaborbalazs/regexp/App.java
+++ src/main/java/hu/gaborbalazs/regexp/App.java
@@ -9,8 +9,8 @@ public class App {
 		System.out.println(text1.matches(pattern1));
 		
 		//Empty String or 193.227.196.129-254
-		String text2 = "";
-		String pattern2 = "^$|193.227.196.(129|1[3-9][0-9]|2[0-4][0-9]|25[0-4])";
+		String text2 = "193.227.196.129";
+		String pattern2 = "^$|193\\.227\\.196\\.(129|1[3-9][0-9]|2[0-4][0-9]|25[0-4])";
 		System.err.println(text2.matches(pattern2));
 	}
 }
