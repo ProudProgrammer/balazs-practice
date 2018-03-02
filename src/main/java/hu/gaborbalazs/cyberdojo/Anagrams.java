606839e2cc03b51f61c1f629d78c3ad221481fbb
gbalazs <gbalazs@localhost>
2018-03-02 15:00:16 +0100

    Anagram and Unit tests

--- src/main/java/hu/gaborbalazs/cyberdojo/Anagrams.java
+++ src/main/java/hu/gaborbalazs/cyberdojo/Anagrams.java
@@ -1,18 +1,62 @@
 package hu.gaborbalazs.cyberdojo;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+
+/**
+ * Write a program to generate all potential 
+ * anagrams of an input string.
+ *
+ * For example, the potential anagrams of "biro" are
+ * 
+ * biro bior brio broi boir bori
+ * ibro ibor irbo irob iobr iorb
+ * rbio rboi ribo riob roib robi
+ * obir obri oibr oirb orbi orib
+ *
+ */
 
 public class Anagrams {
 
     public static void main(String[] args) {
-        
 
+        Anagrams anagrams = new Anagrams();
+        List<String> anagramsList = anagrams.getAnagrams("hgbiro");
+        System.out.println(anagramsList);
+        System.out.println(anagramsList.size());
     }
     
     public List<String> getAnagrams(String input) {
-        List<String> result = new ArrayList<>();
-        return result;
+        if (input == null || input.length() == 0) {
+            return Collections.emptyList();
+        } else if (input.length() == 1) {
+            return Arrays.asList(input);
+        } else if (input.length() > 9) {
+            throw new IllegalArgumentException("Letters element number should be below 10");
+        }
+        return process(Arrays.asList(input.split("")));
     }
 
+    private List<String> process(List<String> letters) {
+        if (letters.size() == 2) {
+            return Arrays.asList(letters.get(0) + letters.get(1), letters.get(1) + letters.get(0));
+        } else {
+            String head;
+            List<String> tail;
+            List<String> result = new ArrayList<>();
+            for (int i = 0; i < letters.size(); i++) {
+                head = letters.get(i);
+                tail = new ArrayList<>(letters);
+                tail.remove(i);
+                tail = process(tail);
+                for (String s : tail) {
+                    result.add(head + s);
+                }
+            }
+            return result;
+        }
+    }
+    
 }
--- src/main/java/hu/gaborbalazs/test/App.java
+++ src/main/java/hu/gaborbalazs/test/App.java
@@ -5,9 +5,6 @@ public class App {
     public static void main(String[] args) {
 
         System.out.println(">> main()");
-        
-        TestDto dto = new TestDto();
-        
 
         System.out.println("<< main()");
     }
