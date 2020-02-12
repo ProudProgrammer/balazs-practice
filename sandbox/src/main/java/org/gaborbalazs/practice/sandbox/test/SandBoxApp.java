cabec8686f53053b15dca85ce05c69db08caec08
Balazs Gabor <v-bgabor@hotels.com>
2020-02-12 17:54:18 +0100

    Java thread puzzle

--- sandbox/src/main/java/org/gaborbalazs/practice/sandbox/test/SandBoxApp.java
+++ sandbox/src/main/java/org/gaborbalazs/practice/sandbox/test/SandBoxApp.java
@@ -2,10 +2,10 @@ package org.gaborbalazs.practice.sandbox.test;
 
 public class SandBoxApp {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		System.out.println(">> main()");
+        System.out.println(">> main()");
 
-		System.out.println("<< main()");
-	}
+        System.out.println("<< main()");
+    }
 }
