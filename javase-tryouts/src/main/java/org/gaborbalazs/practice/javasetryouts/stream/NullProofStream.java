8d0b92aaa3956fb6fe4c3efb29d41c1ea0fb833c
Gábor Balázs <gbazsi82@gmail.com>
2020-08-17 19:57:48 +0200

    Null proof stream processing

---
+++ javase-tryouts/src/main/java/org/gaborbalazs/practice/javasetryouts/stream/NullProofStream.java
@@ -0,0 +1,51 @@
+package org.gaborbalazs.practice.javasetryouts.stream;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+public class NullProofStream {
+
+    public static void main(String[] args) {
+
+        System.out.println(">> main()");
+
+        NullProofStream app = new NullProofStream();
+
+        List<Bundle> list = new ArrayList<>();
+        Bundle bundle = new Bundle();
+        bundle.setNumber(10d);
+        list.add(bundle);
+
+        Double number = app.getNumber(list);
+
+        System.out.println(number);
+
+        System.out.println("<< main()");
+
+    }
+
+    public Double getNumber(List<Bundle> list) {
+        return Optional
+                .ofNullable(list)
+                .orElseGet(Collections::emptyList)
+                .stream()
+                .findFirst()
+                .orElseGet(Bundle::new)
+                .getNumber();
+    }
+}
+
+class Bundle {
+
+    private Double number;
+
+    public Double getNumber() {
+        return number;
+    }
+
+    public void setNumber(Double number) {
+        this.number = number;
+    }
+}
