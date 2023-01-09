962eeb3c5db4392b7edf3dd1148d158ad736658c
Balazs Gabor <balazs.gabor@agcocorp.com>
2023-01-09 14:41:42 +0100

    Fix Hackerrank Loops II

--- coding-practice/src/main/java/org/gaborbalazs/practice/coding/hackerrank/javaloopsii/Solution.java
+++ coding-practice/src/main/java/org/gaborbalazs/practice/coding/hackerrank/javaloopsii/Solution.java
@@ -3,19 +3,17 @@ package org.gaborbalazs.practice.coding.hackerrank.javaloopsii;
 import java.util.Scanner;
 
 class Solution {
+
     public static void main(String[] argh) {
         Scanner in = new Scanner(System.in);
-        int queries = in.nextInt();
-        for (int queryNumber = 0; queryNumber < queries; queryNumber++) {
+        int t = in.nextInt();
+        for (int i = 0; i < t; i++) {
             int a = in.nextInt();
             int b = in.nextInt();
-            int series = in.nextInt();
-            for (int seriesNumber = 0; seriesNumber < series; seriesNumber++) {
-                int seriesResult = a;
-                for (int subSeriesNumber = 0; subSeriesNumber <= seriesNumber; subSeriesNumber++) {
-                    seriesResult += Math.pow(2, subSeriesNumber) * b;
-                }
-                System.out.print(seriesResult + " ");
+            int n = in.nextInt();
+            for (int j = 0; j < n; j++) {
+                a += Math.pow(2, j) * b;
+                System.out.print(a + " ");
             }
             System.out.println();
         }
